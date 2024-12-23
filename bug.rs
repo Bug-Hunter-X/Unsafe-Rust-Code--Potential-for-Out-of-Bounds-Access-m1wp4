6cb0eea24d85a36de235c00096c734abb92b0eda
BugHunter-X <askzenai@gmail.com>
2024-12-23 19:38:04 +0100

    Add bug.rs file

---
+++ bug.rs
@@ -0,0 +1,1 @@
+fn main() {    let mut vec = Vec::new();    vec.push(1);    vec.push(2);    let index = 1;    let element = unsafe { *vec.get_unchecked(index) };    println!("Element at index {}: {}", index, element);}
